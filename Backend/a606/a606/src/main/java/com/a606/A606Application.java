db9596b438c6d95c801cc8ce15cad2dee52aa666
hwi <orien93@naver.com>
2022-09-09 14:29:30 +0900

    [feature] back config

---
+++ Backend/a606/a606/src/main/java/com/a606/A606Application.java
@@ -0,0 +1,13 @@
+package com.a606;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A606Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A606Application.class, args);
+	}
+
+}

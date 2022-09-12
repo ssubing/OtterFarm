45630002c3f84fc3402c84671a56ed8a60f9a64b
subin <5515983@naver.com>
2022-09-12 21:41:46 +0900

    [feature] Add AvatarDetail Page

--- Frontend/src/pages/Welcome/Welcome.jsx
+++ Frontend/src/pages/Welcome/Welcome.jsx
@@ -1,9 +1,47 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+
+import sudal from '../../assets/images/sudal.png'
+
+function MediaCard() {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image={sudal}
+        alt="green iguana"
+      />
+      <CardContent>
+        <div className="info">
+          <span>분양중</span>
+          <span>32</span>
+        </div>
+        <Typography gutterBottom variant="h5" component="div">
+          수달(NFT) 정보
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
 function Welcome() {
   return (
     <div>
       <Navbar />
+      <MediaCard/>
     </div>
   );
 }

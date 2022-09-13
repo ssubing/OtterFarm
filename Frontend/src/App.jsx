5f5324dcfbdfeac3b43a4424380916f533e47e57
LeeSunMin <julia9803@naver.com>
2022-09-13 13:42:31 +0900

    [feature] Set css for Information page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Welcome from "./pages/Welcome/Welcome";
+import Information from "./pages/Information/Information";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Welcome />} />
+      </Routes>
+      <Routes>
+        <Route path="/information" element={<Information />} />
       </Routes>
     </Router>
   );
---
+++ Frontend/src/pages/Information/Information.jsx
@@ -0,0 +1,50 @@
+import Navbar from "../../components/Navbar/Navbar";
+import "./Information.css";
+import Button from "@material-ui/core/Button";
+import game from "../../assets/images/game.png";
+const Information = () => {
+  return (
+    <div className="information">
+      <Navbar></Navbar>
+      <div className="description">
+        <div className="title">수달농장</div>
+        <div className="line"></div>
+        <p className="content">
+          수달농장은 게임을 통해 나만의 수달(NFT)를 만드는 서비스!
+        </p>
+        <div className="title">수달꾸미기</div>
+        <div className="line"></div>
+        <p className="content">모은 수달머니로 당신의 수달을 꾸며보세요</p>
+        <div className="title">수달샵</div>
+        <div className="line"></div>
+        <p className="content">
+          다른 사람들이 키운 수달을 경매를 통해 분양받을 수 있어요!
+        </p>
+        <div className="title">
+          게임하기
+          <Button
+            style={{
+              backgroundColor: "#DAB49D",
+              marginLeft: "10px",
+            }}
+            variant="contained"
+            href="/game"
+          >
+            게임하러 가기
+          </Button>
+        </div>
+        <div className="line"></div>
+        <div className="game">
+          <img src={game} alt="game" className="game-thumbnail"></img>
+          <p className="content">
+            뿌슝빠슝
+            <br />
+            <br />이 게임은 뿌슝빠슝 합니다 😎
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Information;

e3370783e994f1a7c42fea4a7c416fb6082d5bc2
ssubing <5515983@naver.com>
2022-09-13 14:58:31 +0900

    [style] Remove div

--- Frontend/src/pages/Welcome/Welcome.jsx
+++ Frontend/src/pages/Welcome/Welcome.jsx
@@ -5,22 +5,20 @@ import { Link } from "react-router-dom";
 const Welcome = () => {
   return (
     <div className="back">
-      <div>
-        <h1>
-          <div className="waviy">
-            <span></span>
-            <span>수</span>
-            <span>달</span>
-            <span>농</span>
-            <span>장</span>
-          </div>
-        </h1>
-        <div className="mainBtn">
-          <Link to="/main" >
-            <img src={logo} alt="logo" className="logo" />
-            <span>놀러가기</span>
-          </Link>
+      <h1>
+        <div className="waviy">
+          <span></span>
+          <span>수</span>
+          <span>달</span>
+          <span>농</span>
+          <span>장</span>
         </div>
+      </h1>
+      <div className="mainBtn">
+        <Link to="/main" >
+          <img src={logo} alt="logo" className="logo" />
+          <span>놀러가기</span>
+        </Link>
       </div>
     </div>
   )

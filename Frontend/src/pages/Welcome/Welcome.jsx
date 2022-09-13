66b6f8182aae224b89bbc3ed0e64c64ce7fa6b0f
ssubing <5515983@naver.com>
2022-09-13 14:19:43 +0900

    [feature] Add Welcome Page

--- Frontend/src/pages/Welcome/Welcome.jsx
+++ Frontend/src/pages/Welcome/Welcome.jsx
@@ -1,11 +1,28 @@
 import React from "react";
-import Navbar from "../../components/Navbar/Navbar";
-function Welcome() {
+import "./Welcome.css";
+import logo from "../../assets/images/logo.png";
+import { Link } from "react-router-dom";
+const Welcome = () => {
   return (
     <div>
-      <Navbar />
+      <div>
+        <h1>
+          <div className="waviy">
+            <span></span>
+            <span>수</span>
+            <span>달</span>
+            <span>농</span>
+            <span>장</span>
+          </div>
+        </h1>
+        <div className="mainBtn">
+          <Link to="/main" >
+            <img src={logo} alt="logo" className="logo" />
+            <span>놀러가기</span>
+          </Link>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
-
 export default Welcome;

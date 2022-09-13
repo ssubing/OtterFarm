258febe155df6a145dd4a014673a4c3b7263e48e
kyujin <spqj951@gmail.com>
2022-09-13 12:24:35 +0900

    fe/feature/navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Welcome from "./pages/Welcome/Welcome";
+import Item from "./pages/Item/Item";
+import Shop from "./pages/Shop/Shop";
+import Game from "./pages/Game/Game";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Welcome />} />
+        <Route path="/game" element={<Game />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/item" element={<Item />} />
       </Routes>
     </Router>
   );

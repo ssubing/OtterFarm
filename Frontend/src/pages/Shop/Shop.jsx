c7961cc0298e62556a5ea8e2b3d9191779733f14
LeeSunMin <julia9803@naver.com>
2022-09-14 15:44:34 +0900

    [style] change design on tab

--- Frontend/src/pages/Shop/Shop.jsx
+++ Frontend/src/pages/Shop/Shop.jsx
@@ -1,57 +1,31 @@
 import React, { useState } from "react";
-import Navbar from "../../components/Navbar/Navbar";
 import "./Shop.css";
 import { makeStyles } from "@material-ui/core/styles";
+
+// tab
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+
+// menu
 import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
 
+// card
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-
-import avatar from "../../assets/images/avatar.png";
+import avatar from "../../assets/images/찐수달.png";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
 
+// components
+import Navbar from "../../components/Navbar/Navbar";
 import ShowSale from "../../components/Card/ShowSale";
 import Like from "../../components/Card/Like";
-
-const useStyles = makeStyles((theme) => ({
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 120,
-  },
-  selectEmpty: {
-    marginTop: theme.spacing(2),
-  },
-  gridRoot: {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "space-around",
-    overflow: "hidden",
-  },
-  gridList: {
-    // width: 500,
-    // height: 450,
-    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
-    // transform: "translateZ(0)",
-  },
-  root: {
-    width: 345,
-    padding: 30,
-    backgroundColor: "#F3E9DC",
-    borderRadius: "20px",
-  },
-  media: {
-    height: 450,
-    border: "2px solid",
-    borderRadius: "10px",
-    backgroundColor: "#ffffff",
-  },
-}));
 
 const datas = [
   {
@@ -91,12 +65,66 @@ const datas = [
   },
 ];
 
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+  tabRoot: {
+    flexGrow: 1,
+  },
+  appbar: {
+    backgroundColor: "inherit",
+    boxShadow: "none",
+  },
+  tab: {
+    color: "black",
+    fontSize: "20px",
+    fontFamily: "neo",
+  },
+  gridRoot: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    overflow: "hidden",
+  },
+  gridList: {
+    // width: 500,
+    // height: 450,
+    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+    // transform: "translateZ(0)",
+    justifyContent: "space-evenly",
+  },
+  root: {
+    width: 345,
+    padding: 30,
+    backgroundColor: "#F3E9DC",
+    borderRadius: "20px",
+  },
+  media: {
+    height: 400,
+    border: "2px solid",
+    borderRadius: "10px",
+    backgroundColor: "#ffffff",
+  },
+}));
+
 function Shop() {
   const classes = useStyles();
   const [order, setOrder] = useState("");
 
   const handleOrder = (event) => {
     setOrder(event.target.value);
+  };
+
+  const [value, setValue] = useState(0);
+
+  const handleTab = (event, newValue) => {
+    setValue(newValue);
+    // alert(newValue);
   };
 
   return (
@@ -106,11 +134,20 @@ function Shop() {
         <div className="title">수달샵</div>
         <div className="line"></div>
         <div className="wrap">
-          {/* 왼쪽 분양중, 미분양 탭 */}
-          <div className="tab">
-            <span style={{ fontWeight: "bold" }}>전체</span>
-            <span>분양중</span>
-            <span>미분양</span>
+          {/* 왼쪽 탭 */}
+          <div className={classes.tabRoot}>
+            <AppBar position="static" className={classes.appbar}>
+              <Tabs
+                value={value}
+                onChange={handleTab}
+                aria-label="simple tabs example"
+                TabIndicatorProps={{ style: { backgroundColor: "#C08552" } }}
+              >
+                <Tab className={classes.tab} label="전체" />
+                <Tab className={classes.tab} label="분양중" />
+                <Tab className={classes.tab} label="미분양" />
+              </Tabs>
+            </AppBar>
           </div>
           {/* 오른쪽 정렬 기준 */}
           <div className="menu">
@@ -140,7 +177,7 @@ function Shop() {
                 cols={1}
                 style={{
                   height: "auto",
-                  width: "500px",
+                  width: "450px",
                   display: "flex",
                   justifyContent: "center",
                 }}

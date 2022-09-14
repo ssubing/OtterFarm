f8d6fa1ececdb19d5068be633527e9729270ee21
하규진 <spqj951@naver.com>
2022-09-14 15:46:34 +0900

    [FE]/feature/itempage

--- Frontend/src/components/MainNavbar/MainNavbar.jsx
+++ Frontend/src/components/MainNavbar/MainNavbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+
 import "./MainNavbar.css";
 import logo from "../../assets/images/logo.png";
 const MainNavbar = () => {

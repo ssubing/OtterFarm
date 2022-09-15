179621a9ba360c5f9f680b4152e5d94113ff3b17
ssubing <5515983@naver.com>
2022-09-15 22:54:30 +0900

    [feature]Add onsale, bid(request) list component

--- Frontend/src/components/NFTDetail/OnSale.jsx
+++ Frontend/src/components/NFTDetail/OnSale.jsx
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
+import BidList from "./BidList.jsx"
 
+import "./OnSale.css"
+
+//현재 가격과 입찰기록
 const auctionInfo = {
     currentPrice: '1050',
     bidLog: [
@@ -17,8 +21,15 @@ const auctionInfo = {
       }
     ]
 }
-  
+//가격
 function Price() {
+  const [price, setPrice] = useState(0)
+  const bidPriceChange = (e) => {
+    setPrice(e.target.value)
+  }
+  const bidClick = () => {
+    console.log(price)
+  }
     return(
       <div className="price-info">
         <div className="current">
@@ -27,42 +38,31 @@ function Price() {
         </div>
         <div className="bid">
           <span>입찰가</span>
-          <button>입찰</button>
+          <div>
+            <input onChange={bidPriceChange}className="bid-input"/>
+            <button className="bid-btn" onClick={bidClick}>입찰</button>
+          </div>
         </div>
       </div>
     )
 }
   
-function BidList() {
-    const bidLog = auctionInfo.bidLog.map((bid) => 
-        <li key={bid}>{bid.time} {bid.price}</li>
-    )
-    console.log(bidLog)
-    return(
-        <div className="bid-log">
-            <h3>입찰 내역</h3>
-            <div className="bid-title">
-                <span>입찰 시간</span>
-                <span>입찰 가격(SSF)</span>
-            </div>
-            <hr/>
-            <ul>{bidLog}</ul>
-        </div>
-    )
-}
+
 
 function OnSale({start, end}) {
     return(
         <div>
             <div className="sale-info">
                 <h3>분양 정보</h3>
-                <div>
-                    <span>진행기간</span>
+                <div className="auction-date">
+                    <span>진행기간 : </span>
                     <span>{start} ~ {end}</span>
                 </div>
             </div>
+            <hr/>
             <Price/>
-            <BidList/>
+            <hr/>
+            <BidList title="입찰 내역" date="입찰 시간" price="입찰 가격(SSF)" bidLog={auctionInfo.bidLog}/>
         </div>
     )
 }

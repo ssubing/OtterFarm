e50c91dffab53d3b1a871db2937e487b854d918d
ssubing <5515983@naver.com>
2022-09-15 22:50:23 +0900

    [feature] Add AvatarDetail Page

--- Frontend/src/pages/Avatar/AvatarDetail.jsx
+++ Frontend/src/pages/Avatar/AvatarDetail.jsx
@@ -1,47 +1,13 @@
 import React from "react";
-
 import Navbar from "../../components/Navbar/Navbar";
 
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-
-import sudal from '../../assets/images/sudal.png'
-
-import OnSale from "../../components/NFTInfo/OnSale";
+import OnSale from "../../components/NFTDetail/OnSale";
+import NFTInfo from "../../components/NFTDetail/NFTInfo";
+import UnSoldOwner from "../../components/NFTDetail/UnSoldOwner"
 
 import "./AvatarDetail.css"
 
-function MediaCard() {
-  return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        height="140"
-        image={sudal}
-        alt="green iguana"
-      />
-      <CardContent>
-        <div className="info">
-          <div>분양중</div>
-          <div>32</div>
-        </div>
-        <Typography gutterBottom variant="h5" component="div">
-          수달(NFT) 정보
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-    </Card>
-  );
-}
+
 
 const avatarInfo = {
   owner: '이선민',
@@ -54,12 +20,13 @@ const avatarInfo = {
 
 function AvatarDetail() {
   return (
-    <div>
+    <div className="avatar-wrap">
       <Navbar/>
-      <div className="content">
-        <MediaCard/>
-        <div>
-          <OnSale {...avatarInfo}/>
+      <div className="detail-content">
+        <NFTInfo {...avatarInfo}/>
+        <div className="sale-wrap">
+          {/* <OnSale {...avatarInfo}/> */}
+          <UnSoldOwner/>
         </div>
       </div>
     </div>

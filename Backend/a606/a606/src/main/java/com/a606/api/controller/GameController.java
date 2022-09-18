407cec13d3631893651832df1f295ae0a40ffb5f
hwi <orien93@naver.com>
2022-09-18 21:17:56 +0900

    [feature] Game,Shop Controller

--- Backend/a606/a606/src/main/java/com/a606/api/controller/GameController.java
+++ Backend/a606/a606/src/main/java/com/a606/api/controller/GameController.java
@@ -1,4 +1,29 @@
 package com.a606.api.controller;
 
+import com.a606.api.service.GameService;
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@Api(value = "게임 API", tags = {"Game"})
+@RestController
+@RequestMapping("api/")
+@RequiredArgsConstructor
 public class GameController {
+
+    GameService gameService;
+
+    @GetMapping("game/point/{userId}")
+    public ResponseEntity<Long> getGamePoint(@PathVariable long userId) {
+        return new ResponseEntity<Long>(gameService.getGamePointById(userId), HttpStatus.OK);
+    }
+
+    @PutMapping("game/point/{userId}/{point}")
+    public ResponseEntity<Long> updateGamePoint(@PathVariable long userId, @PathVariable long point) {
+        return new ResponseEntity<Long>(gameService.updateGamePointById(userId, point),HttpStatus.OK);
+    }
 }
--- Backend/a606/a606/src/main/java/com/a606/api/controller/ShopController.java
+++ Backend/a606/a606/src/main/java/com/a606/api/controller/ShopController.java
@@ -1,4 +1,19 @@
 package com.a606.api.controller;
 
+import com.a606.api.service.ShopService;
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@Api(value = "상점 API", tags = {"Shop"})
+@RestController
+@RequestMapping("api/")
+@RequiredArgsConstructor
 public class ShopController {
+    ShopService shopService;
+
+
 }

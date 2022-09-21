39345515bb4ede2b53180daaba150e4352b2d646
하규진 <spqj951@naver.com>
2022-09-21 15:43:50 +0900

    [FE]/feature/아바타 꾸미기

--- Frontend/src/components/MyItem/MyItem.jsx
+++ Frontend/src/components/MyItem/MyItem.jsx
@@ -3,48 +3,45 @@ import ReactPaginate from "react-paginate";
 import "./MyItem.css";
 
 
-const imageUrl = "../../assets/images/items/Cloth/"
+
 const items = [
+  
   {
-    url :require("../../assets/images/items/Cloth/cloth_01.png"),
-    name : "cloth01"
+    url: require("../../assets/images/items/Head/head_01.png"),
+    name: "head01"
   },
   {
-    url:require("../../assets/images/items/Cloth/cloth_02.png"),
-    name:"cloth02"
+    url: require("../../assets/images/items/Head/head_02.png"),
+    name: "head02"
   },
   {
-    url :require("../../assets/images/items/Cloth/cloth_03.png"),
-    name : "cloth03"
+    url: require("../../assets/images/items/Head/head_03.png"),
+    name: "head03"
   },
   {
-    url :require("../../assets/images/items/Cloth/cloth_04.png"),
-    name : "cloth04"
+    url: require("../../assets/images/items/Head/head_04.png"),
+    name: "head04"
   },
   {
-    url :require("../../assets/images/items/Cloth/cloth_05.png"),
-    name : "cloth05"
+    url: require("../../assets/images/items/Head/head_05.png"),
+    name: "head05"
   },
   {
-    url :require("../../assets/images/items/Cloth/cloth_06.png"),
-    name : "cloth06"
+    url: require("../../assets/images/items/Head/head_06.png"),
+    name: "head06"
   },
   {
-    url :require("../../assets/images/items/Cloth/cloth_07.png"),
-    name : "cloth07"
+    url: require("../../assets/images/items/Head/head_07.png"),
+    name: "head07"
   },
   {
-    url :require("../../assets/images/items/Cloth/cloth_08.png"),
-    name : "cloth08"
-  },
-  {
-    url :require("../../assets/images/items/Cloth/cloth_09.png"),
-    name : "cloth09"
+    url: require("../../assets/images/items/Head/head_08.png"),
+    name: "head08"
   },
 ];
 
-function Cloth({ itemsPerPage,setUrl}) {
-  const [currentItems, setCurrentItems] = useState(items);
+function MyItem({ itemsPerPage,setUrl}) {
+  const [currentItems, setCurrentItems] = useState(null);
   const [pageCount, setPageCount] = useState(0);
   const [itemOffset, setItemOffset] = useState(0);
 
@@ -68,14 +65,14 @@ function Cloth({ itemsPerPage,setUrl}) {
   return (
     <div className="inventory">
       <div className="items">
-     {currentItems.map((item)=>( <img className="parts" key={item.name} src={item.url} onClick={() => setUrl(item.url)}/>) )}
+     {items.map((item)=>( <img className="parts" src={item.url} onClick={() => setUrl(item.url)}/>) )}
      </div>
       <ReactPaginate
         className="paginate"
         breakLabel="..."
         nextLabel=">"
         onPageChange={handlePageClick}
-        pageRangeDisplayed={8}
+        pageRangeDisplayed={1}
         pageCount={pageCount}
         previousLabel="<"
         renderOnZeroPageCount={null}
@@ -84,4 +81,4 @@ function Cloth({ itemsPerPage,setUrl}) {
   );
 }
 
-export default Cloth;
+export default MyItem;
--- Frontend/src/pages/Item/Item.jsx
+++ Frontend/src/pages/Item/Item.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import "./Item.css";
 import Canvas from "../../components/Canvas/Canvas";
 import { useState } from "react";
 import otter from "../../assets/images/otter.png";
-import MyItem from "../../components/MyItem/Head";
+import MyItem from "../../components/MyItem/MyItem";
+import Eyes from "../../components/MyItem/Eyes";
+import Mouth from "../../components/MyItem/Mouth";
+import Hands from "../../components/MyItem/Hands";
+import Cloth from "../../components/MyItem/Cloth";
 function Item() {
   const [itemNum, setItemNum] = useState(-1);
   let data = [
@@ -21,6 +25,9 @@ function Item() {
 
   const parts = ['머리', '눈', '입', '손', '옷'];
   const [selected,setSelected] = useState(Array(parts.length).fill(false));
+  useEffect(()=> {
+    setSelected([false,false,false,false,true])
+  },[])
   const pickedItem = data[itemNum];
   const onClickHandler = () => {
     setItemNum([Math.floor(Math.random() * data.length)]);
@@ -28,6 +35,10 @@ function Item() {
   };
 
   const [url, setUrl] = useState();
+  const [eyeUrl, setEyeUrl] = useState();
+  const [mouthUrl, setMouthUrl] = useState();
+  const [handUrl, setHandUrl] = useState();
+  const [clothUrl, setClothUrl] = useState();
   const [clicked, setClicked] = useState(false);
   const onClick = (idx) => {
         const newArr = Array(parts.length).fill(false);
@@ -41,11 +52,39 @@ function Item() {
       <div className="itemPage">
         <div className="myNft">
           {/* <img src={otter} alt="basic otter" className="otter"/> */}
-          <Canvas url={otter} width={300} height={400} url2={url} />
+          <div className="otterNft">
+            <img className="char base" src={require("../../assets/images/otter.png")}/>
+            <img className="char head" src={url}/>
+            <img className="char eye" src={eyeUrl}/>
+            <img className="char mouth" src={mouthUrl}/>
+            <img className="char hand" src={handUrl}/>
+            <img className="char cloth" src={clothUrl}/>
+          </div>
           <div className="select">
           {parts.map((part, idx)=> <div className={"part " + (selected[idx]? "chosen" : null)} key={idx} onClick={() => onClick(idx)}>{part}</div> )}
           </div>
-          <MyItem itemsPerPage={8} settingUrl={setUrl} />
+          {selected[0] ? 
+          <MyItem itemsPerPage={4} setUrl={setUrl}/>:
+          selected[1]?
+          <Eyes  itemsPerPage={4} setUrl={setEyeUrl}/> :
+          selected[2]?
+          <Mouth  itemsPerPage={4} setUrl={setMouthUrl}/> :
+          selected[3] ?
+          <Hands itemsPerPage={4} setUrl={setHandUrl}/>:
+          <Cloth itemsPerPage={8} setUrl={setClothUrl}/>
+}
+          
+          {/* <div className="inventory">
+          {selected[0]? 
+          <Head itemsPerPage={8} settingUrl={setUrl} /> :
+          selected[1] ?
+          <Eyes itemsPerPage={8} settingUrl={setUrl} /> :
+          selected[2] ?
+          <Mouth itemsPerPage={8} settingUrl={setUrl} /> :
+          selected[3] ?
+          <Hands itemsPerPage={8} settingUrl={setUrl} /> :
+          <Cloth itemsPerPage={8} settingUrl={setUrl} /> }
+          </div> */}
         </div>
         <div className="itemShop">
           아이템들

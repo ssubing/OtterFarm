f7fdd638c3a8b42e8e70799decd4bfdba9f73ce2
kyujin <spqj951@gmail.com>
2022-09-23 11:20:18 +0900

    feat: 랜덤박스 뽑기 기능

--- Frontend/src/components/RandomBox/RandomBox.jsx
+++ Frontend/src/components/RandomBox/RandomBox.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
-import {motion} from "framer-motion";
-function RandomBox() {
-  return (
-    <motion.div>RandomBox</motion.div>
-  )
-}
+import React, { useEffect, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import "./RandomBox.css";
+const backdrop = {
+  visible: { opacity: 1 },
+  hidden: { opacity: 0 },
+};
 
-export default RandomBox
+const RandomBox = ({ showBox, setShowBox, part, showImg, setShowImg }) => {
+  return (
+    <AnimatePresence exitBeforeEnter>
+      {showBox && (
+        <motion.div
+          className="backdrop"
+          variants={backdrop}
+          initial="hidden"
+          animate="visible"
+        >
+          <div className="modal">
+            {!showImg ? (
+              <img
+                className="giftBox"
+                alt="giftbox"
+                src={require("../../assets/images/present.png")}
+              />
+            ) : (
+              <div>
+                <img
+                  className="gottenItem"
+                  src={require("../../assets/images/items/Cloth/cloth_01.png")}
+                  alt="gottenItem"
+                />
+                <button
+                  onClick={() => {
+                    setShowBox(false);
+                    setShowImg(false);
+                  }}
+                >
+                  확인
+                </button>
+              </div>
+            )}
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default RandomBox;
--- Frontend/src/pages/Item/Item.jsx
+++ Frontend/src/pages/Item/Item.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import "./Item.css";
-import Canvas from "../../components/Canvas/Canvas";
+
 import { useState } from "react";
-import otter from "../../assets/images/otter.png";
+
 import MyItem from "../../components/MyItem/MyItem";
 import Eyes from "../../components/MyItem/Eyes";
 import Mouth from "../../components/MyItem/Mouth";
 import Hands from "../../components/MyItem/Hands";
 import Cloth from "../../components/MyItem/Cloth";
+
+import RandomBox from "../../components/RandomBox/RandomBox";
+import { flexbox } from "@mui/system";
 function Item() {
   const [itemNum, setItemNum] = useState(-1);
   let data = [
@@ -23,72 +26,214 @@ function Item() {
     "멍청한 눈알",
   ];
 
-  const parts = ['머리', '눈', '입', '손', '옷'];
-  const [selected,setSelected] = useState(Array(parts.length).fill(false));
-  useEffect(()=> {
-    setSelected([false,false,false,false,true])
-  },[])
-  const pickedItem = data[itemNum];
-  const onClickHandler = () => {
-    setItemNum([Math.floor(Math.random() * data.length)]);
-    console.log(itemNum);
-  };
+  const parts = ["머리", "눈", "입", "손", "옷"];
+  const [selected, setSelected] = useState(Array(parts.length).fill(false));
+  useEffect(() => {
+    setSelected([false, false, false, false, true]);
+  }, []);
 
   const [url, setUrl] = useState();
   const [eyeUrl, setEyeUrl] = useState();
   const [mouthUrl, setMouthUrl] = useState();
   const [handUrl, setHandUrl] = useState();
   const [clothUrl, setClothUrl] = useState();
-  const [clicked, setClicked] = useState(false);
+
+  const [showBox, setShowBox] = useState(false);
+  const [part, setPart] = useState("");
   const onClick = (idx) => {
-        const newArr = Array(parts.length).fill(false);
-        newArr[idx] = true;
-        setSelected(newArr);
-  }
-  
+    const newArr = Array(parts.length).fill(false);
+    newArr[idx] = true;
+    setSelected(newArr);
+  };
+  const [showImg, setShowImg] = useState(false);
+
   return (
     <div className="pageBox">
       <Navbar />
       <div className="itemPage">
         <div className="myNft">
-          {/* <img src={otter} alt="basic otter" className="otter"/> */}
           <div className="otterNft">
-            <img className="char base" src={require("../../assets/images/otter.png")}/>
-            <img className="char head" src={url}/>
-            <img className="char eye" src={eyeUrl}/>
-            <img className="char mouth" src={mouthUrl}/>
-            <img className="char hand" src={handUrl}/>
-            <img className="char cloth" src={clothUrl}/>
+            <img
+              className="char base"
+              src={require("../../assets/images/otter.png")}
+              alt="base"
+            />
+            <img className="char head" src={url} alt="head" />
+            <img className="char eye" src={eyeUrl} alt="eye" />
+            <img className="char mouth" src={mouthUrl} alt="mouth" />
+            <img className="char hand" src={handUrl} alt="hand" />
+            <img className="char cloth" src={clothUrl} alt="cloth" />
           </div>
           <div className="select">
-          {parts.map((part, idx)=> <div className={"part " + (selected[idx]? "chosen" : null)} key={idx} onClick={() => onClick(idx)}>{part}</div> )}
+            {parts.map((part, idx) => (
+              <div
+                className={"part " + (selected[idx] ? "chosen" : null)}
+                key={idx}
+                onClick={() => onClick(idx)}
+              >
+                {part}
+              </div>
+            ))}
           </div>
-          {selected[0] ? 
-          <MyItem itemsPerPage={4} setUrl={setUrl}/>:
-          selected[1]?
-          <Eyes  itemsPerPage={4} setUrl={setEyeUrl}/> :
-          selected[2]?
-          <Mouth  itemsPerPage={4} setUrl={setMouthUrl}/> :
-          selected[3] ?
-          <Hands itemsPerPage={4} setUrl={setHandUrl}/>:
-          <Cloth itemsPerPage={8} setUrl={setClothUrl}/>
-}
-          
-          {/* <div className="inventory">
-          {selected[0]? 
-          <Head itemsPerPage={8} settingUrl={setUrl} /> :
-          selected[1] ?
-          <Eyes itemsPerPage={8} settingUrl={setUrl} /> :
-          selected[2] ?
-          <Mouth itemsPerPage={8} settingUrl={setUrl} /> :
-          selected[3] ?
-          <Hands itemsPerPage={8} settingUrl={setUrl} /> :
-          <Cloth itemsPerPage={8} settingUrl={setUrl} /> }
-          </div> */}
+          {selected[0] ? (
+            <MyItem itemsPerPage={4} setUrl={setUrl} />
+          ) : selected[1] ? (
+            <Eyes itemsPerPage={8} setUrl={setEyeUrl} />
+          ) : selected[2] ? (
+            <Mouth itemsPerPage={8} setUrl={setMouthUrl} />
+          ) : selected[3] ? (
+            <Hands itemsPerPage={8} setUrl={setHandUrl} />
+          ) : (
+            <Cloth itemsPerPage={8} setUrl={setClothUrl} />
+          )}
         </div>
+
         <div className="itemShop">
-          아이템들
-          <button onClick={onClickHandler}>뽑기</button>
+          <div className="shopName">아이템 상점</div>
+          <div className="shoppart">
+            <RandomBox
+              showBox={showBox}
+              setShowBox={setShowBox}
+              part={part}
+              showImg={showImg}
+              setShowImg={setShowImg}
+            />
+            <div
+              className="giftDiv"
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <span>머리</span>
+              <img
+                className="giftbox"
+                alt="giftbox"
+                src={require("../../assets/images/present.png")}
+                style={{ width: "30%" }}
+              />
+              <button
+                onClick={() => {
+                  setShowBox(true);
+                  setPart("01");
+                  setTimeout(() => {
+                    setShowImg(true);
+                  }, 4000);
+                }}
+              >
+                뽑기
+              </button>
+            </div>
+            <div
+              className="giftDiv"
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <span>눈</span>
+              <img
+                className="giftbox"
+                alt="giftbox"
+                src={require("../../assets/images/present.png")}
+                style={{ width: "30%" }}
+              />
+              <button
+                onClick={() => {
+                  setShowBox(true);
+                  setPart("02");
+                  setTimeout(() => {
+                    setShowImg(true);
+                  }, 4000);
+                }}
+              >
+                뽑기
+              </button>
+            </div>
+            <div
+              className="giftDiv"
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <span>입</span>
+              <img
+                className="giftbox"
+                alt="giftbox"
+                src={require("../../assets/images/present.png")}
+                style={{ width: "30%" }}
+              />
+              <button
+                onClick={() => {
+                  setShowBox(true);
+                  setPart("03");
+                  setTimeout(() => {
+                    setShowImg(true);
+                  }, 4000);
+                }}
+              >
+                뽑기
+              </button>
+            </div>
+            <div
+              className="giftDiv"
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <span>손</span>
+              <img
+                className="giftbox"
+                alt="giftbox"
+                src={require("../../assets/images/present.png")}
+                style={{ width: "30%" }}
+              />
+              <button
+                onClick={() => {
+                  setShowBox(true);
+                  setPart("04");
+                  setTimeout(() => {
+                    setShowImg(true);
+                  }, 4000);
+                }}
+              >
+                뽑기
+              </button>
+            </div>
+            <div
+              className="giftDiv"
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <span>옷</span>
+              <img
+                className="giftbox"
+                alt="giftbox"
+                src={require("../../assets/images/present.png")}
+                style={{ width: "30%" }}
+              />
+              <button
+                onClick={() => {
+                  setShowBox(true);
+                  setPart("05");
+                  setTimeout(() => {
+                    setShowImg(true);
+                  }, 4000);
+                }}
+              >
+                뽑기
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>

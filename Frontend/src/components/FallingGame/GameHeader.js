401c0804be46503bb0c76d2b2b46ffc4e292abde
LeeSunMin <julia9803@naver.com>
2022-09-27 14:40:42 +0900

    [feat] 게임 헤더 추가
    게임 화면 상단에 게임 이름과 게임 목록 버튼을 보여주는 헤더 추가

---
+++ Frontend/src/components/FallingGame/GameHeader.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+  header: {
+    display: "flex",
+    justifyContent: "space-between",
+    width: "80%",
+    margin: "0 auto",
+  },
+}));
+function GameHeader(props) {
+  const classes = useStyles();
+  return (
+    <div className={classes.header}>
+      <div className="game-title">{props.title}</div>
+      <Button
+        style={{
+          fontFamily: "neo",
+          fontWeight: "bold",
+          backgroundColor: "#DAB49D",
+          marginLeft: "10px",
+        }}
+        variant="contained"
+        href="/game"
+      >
+        목록
+      </Button>
+    </div>
+  );
+}
+
+export default GameHeader;

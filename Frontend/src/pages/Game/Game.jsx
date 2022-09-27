4a8d49e9bac0c0c1491efbb2f87d98059cfd1c00
ssubing <5515983@naver.com>
2022-09-27 15:58:01 +0900

    [feat] 게임 페이지에 카드 게임 움짤 추가

--- Frontend/src/pages/Game/Game.jsx
+++ Frontend/src/pages/Game/Game.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import cardgame from '../../assets/images/cardgame.gif'
+import fallinggame from '../../assets/images/fallinggame.gif'
+import { Link } from "react-router-dom";
 
 function Game() {
-  return <div>Game Page</div>;
+  return(
+    <div>
+      <h1>game page</h1>
+      <Link to="/cardGame" >
+        <img src={cardgame} alt="카드게임" />
+      </Link>
+      <Link to="/cardGame" >
+        <img src={fallinggame} alt="카드게임" />
+      </Link>
+    </div>
+  )
 }
 
 export default Game;

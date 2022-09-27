3a8b018c47a31baf685eaf008e4caef0d3d42a09
LeeSunMin <julia9803@naver.com>
2022-09-27 15:34:01 +0900

    [feat] 낙하게임 움짤 추가
    게임 목록 화면에 보여줄 움짤 추가

--- Frontend/src/pages/Game/Game.jsx
+++ Frontend/src/pages/Game/Game.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import fallingGame from "../../assets/images/falling-game.gif";
 
 function Game() {
-  return <div>Game Page</div>;
+  return (
+    <div>
+      <h1>game page</h1>
+      <Link to="/fallingGame">
+        <img src={fallingGame} alt="낙하 게임" />
+      </Link>
+    </div>
+  );
 }
 
 export default Game;

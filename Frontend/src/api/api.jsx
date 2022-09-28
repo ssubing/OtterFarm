91bba6738c3dc83f96858b9665f7c9233c909a53
LeeSunMin <julia9803@naver.com>
2022-09-28 15:23:28 +0900

    [feat] API 기본 세팅
    - base api 파일 및 폴더 구조 세팅
    - 수달샵에 사용할 api 등록

---
+++ Frontend/src/api/api.jsx
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const HOST = "http://j7a606.p.ssafy.io:8080/api";
+
+axios.defaults.withCredentials = true;
+const api = axios.create({
+  baseURL: HOST,
+});
+
+export default api;

da8c4d971c9ab67e97b76fcbf11070f6a5912304
kyujin <spqj951@gmail.com>
2022-09-29 11:15:15 +0900

    feat: 테트리스 만들기

--- Frontend/src/components/TetrisGame/TetrisGame.jsx
+++ Frontend/src/components/TetrisGame/TetrisGame.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from "react";
 import Menu from "../../components/Menu/Menu";
-const TetrisGame = ({rows, columns}) => {
-    const start = () => {
-        console.log("start")
-    }
-    return (
-        <div className="TetrisGame">
-            <Menu onClick = {start}/>
-           </div>
-    )
-}
+import { useGameOver } from "../../hooks/useGameOver";
+const TetrisGame = ({ rows, columns }) => {
+  const [gameOver, setGameOver, resetGameOver] = useGameOver();
+  const start = () => {
+    console.log("start");
+  };
+  return (
+    <div className="TetrisGame">
+      <Menu onClick={start} />
+    </div>
+  );
+};
 
-export default TetrisGame
+export default TetrisGame;
---
+++ Frontend/src/hooks/useGameOver.js
@@ -0,0 +1,9 @@
+import React, { useState, useCallback } from "react";
+
+export const useGameOver = () => {
+  const [gameOver, setGameOver] = useState(true);
+  const resetGameOver = useCallback(() => {
+    setGameOver(false);
+  }, []);
+  return [gameOver, setGameOver, resetGameOver];
+};

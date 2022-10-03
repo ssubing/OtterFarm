80b68f344038c7def32cd87bb9b3efe7030c4b60
이상권 <copeve@naver.com>
2022-10-04 03:30:23 +0900

    feat: NFT 경매 contract 조회, 입찰, 경매 완료 추가

--- Solidity/contracts/SudalAuction.sol
+++ Solidity/contracts/SudalAuction.sol
@@ -15,6 +15,9 @@ contract SudalAuction is Ownable {
     // 토큰 id와 입찰 매핑
     mapping(uint => Bid[]) public bids;
 
+    // 토큰 id와 입찰 인덱스 매핑
+    mapping(uint => uint) public bidIndexes;
+
 
     struct Auction {
         address seller;
@@ -59,31 +62,40 @@ contract SudalAuction is Ownable {
         newAuction.isActive = true;
 
         auctions[_tokenId] = newAuction;
+        bidIndexes[_tokenId] = 0;
 
         // emit AuctionCreated(msg.sender, _tokenId);
         return true;
     }
 
+    function getAuctionByTokenId(uint _tokenId) external view returns(address, uint, uint, uint, bool) {
+        Auction memory auction = auctions[_tokenId];
+        return (auction.seller, auction.start, auction.end, auction.firstPrice, auction.isActive);
+    }
+
     function bidOnAuction(uint _tokenId, uint _price) public payable {
-        IERC20 erc20 = IERC20(_sudalFarmContractAddress);
+        IERC20 erc20 = IERC20(_erc20ContractAddress);
 
         // 경매 마감 전인지 확인
         require(auctions[_tokenId].end > block.timestamp);
         // 토큰이 충분한지 확인
         require(erc20.balanceOf(msg.sender) > _price);
 
+        Bid[] memory bidArray = bids[_tokenId];
+        uint bidIndex = bidIndexes[_tokenId];
+
         // 시작가 또는 마지막 입찰가를 넘겼는지 확인
-        if (bids.length == 0) {
-            require(_price > nftAuction[_tokenId].firstPrice);
+        if (bidIndex == 0) {
+            require(_price > auctions[_tokenId].firstPrice);
         } else {
-            require(_price > bids[bids.length - 1].price);
+            require(_price > bidArray[bidIndex - 1].price);
         }
 
         erc20.transferFrom(msg.sender, address(this), _price);
 
         // 마지막 입찰 환불
-        if (bids.length > 0) {
-            lastBid = bids[bids.length - 1];
+        if (bidIndex > 0) {
+            Bid memory lastBid = bidArray[bidIndex - 1];
             erc20.transfer(lastBid.bidder, lastBid.price);
         }
 
@@ -93,7 +105,26 @@ contract SudalAuction is Ownable {
         newBid.bidTime = block.timestamp;
         newBid.price = _price;
 
-        auctionBids[_tokenId].push(newBid);
+        if (bids[_tokenId].length > bidIndex) {
+            bids[_tokenId][bidIndex] = newBid;
+        } else {
+            bids[_tokenId].push(newBid);
+        }
+
+        bidIndexes[_tokenId] = bidIndex + 1;
+    }
+
+    function getBidByTokenId(uint _tokenId) external view returns(address[] memory, uint[] memory, uint[] memory) {
+        address[] memory bidders = new address[](bidIndexes[_tokenId]);
+        uint[] memory bidTimes = new uint[](bidIndexes[_tokenId]);
+        uint[] memory prices = new uint[](bidIndexes[_tokenId]);
+
+        for (uint i = 0; i < bidIndexes[_tokenId]; i++){
+            bidders[i] = bids[_tokenId][i].bidder;
+            bidTimes[i] = bids[_tokenId][i].bidTime;
+            prices[i] = bids[_tokenId][i].price;
+        }
+        return(bidders, bidTimes, prices);
     }
 
     function approveAndTransfer(address _from, address _to, uint _tokenId) internal returns(bool) {
@@ -105,9 +136,29 @@ contract SudalAuction is Ownable {
 
     function endAuction(uint _tokenId) public {
         Auction memory auction = auctions[_tokenId];
-        Bid[] memory bid = bids[_tokenId];
+        Bid[] memory bidArray = bids[_tokenId];
+        uint bidIndex = bidIndexes[_tokenId];
 
-        approveAndTransfer(address(this), auction.seller, _tokenId);
+        // 경매 중인지 확인
+        require(auction.isActive);
+        // 경매 마감 시간이 지났는지 확인
+        require(block.timestamp > auction.end);
+
+         // 입찰이 있는 경우
+        if (bidIndex > 0) {
+            // 판매자에게 토큰 전송
+            IERC20(_erc20ContractAddress).transfer(auction.seller, bidArray[bidIndex - 1].price);
+
+            // 구매자에게 NFT 전송
+            approveAndTransfer(address(this), bidArray[bidIndex - 1].bidder, _tokenId);
+        }
+        // 입찰이 없는 경우
+        else {
+            // 원래 주인에게 NFT 전송
+            approveAndTransfer(address(this), auction.seller, _tokenId);
+        }
+
+        auctions[_tokenId].isActive = false;
     }
 
 }

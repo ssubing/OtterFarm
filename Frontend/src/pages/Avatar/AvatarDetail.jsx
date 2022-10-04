0eec014a62f847ec7ac5b5e1d03b71f1793a9b36
ssubing <5515983@naver.com>
2022-10-04 11:26:59 +0900

    [feat] 로컬 스토리지에 userId도 저장 & 토큰 저장 내용 수정

--- Frontend/src/pages/Avatar/AvatarDetail.jsx
+++ Frontend/src/pages/Avatar/AvatarDetail.jsx
@@ -14,15 +14,6 @@ import { useLocation } from "react-router-dom";
 import shop from "../../api/shop";
 import { setNftDetailOne } from "../../store/modules/shop";
 
-const avatarInfo = {
-  owner: "이선민",
-  address: "0x123515adfasdfbaf44ea907c797788d8dbf",
-  start: "2022-09-01 12:00",
-  end: "2022-09-04 12:00",
-  likeCnt: "20",
-  isSale: true,
-};
-
 function AvatarDetail() {
   const location = useLocation();
 
@@ -34,6 +25,7 @@ function AvatarDetail() {
     shop
       .nftDetailOne(nftId)
       .then((result) => {
+        console.log(result.data)
         dispatch(setNftDetailOne(result.data));
       })
       .catch((error) => {

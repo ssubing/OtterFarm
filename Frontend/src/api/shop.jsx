84682a576a21cd3ebd20d50a3191bd87e27e2b91
ssubing <5515983@naver.com>
2022-10-04 16:31:24 +0900

    [feat] 좋아요 API 연결

--- Frontend/src/api/shop.jsx
+++ Frontend/src/api/shop.jsx
@@ -3,6 +3,7 @@ import api from "./api";
 const END_POINT = "board";
 
 const shop = {
+  //nft 전체 목록 조회(수달샵)
   nftList(params) {
     return api({
       method: "get",
@@ -10,16 +11,29 @@ const shop = {
       params: params,
     });
   },
+  //nft 상세 조회
   nftDetailOne(params) {
     return api({
       method: "get",
       url: `${END_POINT}/details/${params}`
     })
   },
+  //미분양 nft 요청 내역 정보 조회
   nftUnsoldOne(params) {
     return api({
       method: "get",
       url: `${END_POINT}/appeal/${params}`
+    })
+  },
+  //좋아요 클릭
+  nftLikeOne(params) {
+    console.log(params)
+    const formData = new FormData()
+    formData.append('nftId', params.nftId)
+    return api({
+      method: "post",
+      url: `${END_POINT}/likes`,
+      data: formData
     })
   }
 };
--- Frontend/src/components/Card/Like.jsx
+++ Frontend/src/components/Card/Like.jsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+// import { useDispatch, useSelector } from "react-redux";
+import shop from "../../api/shop";
+// import { setNftLikeOne } from "../../store/modules/shop";
 
 function Like(props) {
+  const nftId = props.nftId 
+  //좋아요 클릭
+  const likeClick = () => {
+    // console.log(likeFlag)
+    // console.log(props.likeCnt)
+    const params = {
+      nftId : nftId,
+      likeCount : props.likeCnt
+    }
+      shop
+      .nftLikeOne(params)
+      .then((result) => {
+          // dispatch(setNftLikeOne(result.data))
+          console.log(result)
+      })
+      .catch((error) => {
+          console.log(error);
+      });
+    
+  }
+  
   return (
-    <div style={{ display: "flex", alignItems: "center", fontSize: "20px" }}>
+    <div style={{ display: "flex", alignItems: "center", fontSize: "20px" }} onClick={likeClick}>
       {props.liked ? (
         <FavoriteIcon style={{ color: "red", marginRight: "5px" }} />
       ) : (
         <FavoriteBorderIcon style={{ color: "red", marginRight: "5px" }} />
       )}
-      
       {props.likeCnt}
     </div>
   );
